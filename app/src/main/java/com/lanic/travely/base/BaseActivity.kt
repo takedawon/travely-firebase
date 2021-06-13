1c307def6414d429e83f16795cba3a21a1797566
lanic <takedawon@naver.com>
2021-06-13 23:45:56 +0900

    Base 작업

---
+++ app/src/main/java/com/lanic/travely/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.lanic.travely.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(
+    @LayoutRes val layoutId: Int
+) : AppCompatActivity() {
+
+    lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+    }
+}
