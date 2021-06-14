d2955943c44244b8cf148d4e5ae913aad83c8df7
lanic <takedawon@naver.com>
2021-06-15 00:10:35 +0900

    navigation 연결 및 아이콘 저장

--- app/src/main/java/com/lanic/travely/HomeFragment.kt
+++ app/src/main/java/com/lanic/travely/HomeFragment.kt
@@ -10,16 +10,10 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
     layoutId = R.layout.fragment_home
 ) {
 
-    private val viewController by lazy {
-        MainPostEpoxyController()
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
-            rvMain.adapter = viewController.adapter
-            rvMain.layoutManager = LinearLayoutManager(requireContext())
         }
     }
 }
--- app/src/main/java/com/lanic/travely/MainActivity.kt
+++ app/src/main/java/com/lanic/travely/MainActivity.kt
@@ -2,10 +2,14 @@ package com.lanic.travely
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lanic.travely.base.BaseActivity
+import com.lanic.travely.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    layoutId = R.layout.activity_main
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
