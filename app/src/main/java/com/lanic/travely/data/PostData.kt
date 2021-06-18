f515ea69db8b414ad45e084ff8cd0d95d9e28ae1
lanic <takedawon@naver.com>
2021-06-18 21:37:48 +0900

    LoginActivity 디자인 작업중

--- app/src/main/java/com/lanic/travely/data/PostData.kt
+++ app/src/main/java/com/lanic/travely/data/PostData.kt
@@ -3,7 +3,7 @@ package com.lanic.travely.data
 data class PostData(
     val nickname: String,
     val destination: String,
-    val photoUrlList: ArrayList<String>
+    val photoUrlList: ArrayList<String>,
     val content: String,
     val commentCount: Int,
     val likeCount: Int
---
+++ app/src/main/java/com/lanic/travely/ui/SnsButton.kt
@@ -0,0 +1,65 @@
+package com.lanic.travely.ui
+
+import android.app.Service
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.lanic.travely.R
+
+class SnsButton : LinearLayout {
+
+    @JvmOverloads
+    constructor(context: Context, attributeSet: AttributeSet? = null, defStyleAttr: Int = 0) : super(
+        context,
+        attributeSet,
+        defStyleAttr
+    ) {
+        initView()
+        if (attributeSet != null) {
+            getAttrs(attributeSet, defStyleAttr)
+        }
+    }
+
+    private fun getAttrs(attributeSet: AttributeSet, defStyleAttr: Int) {
+        val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.SnsButton, defStyleAttr, 0)
+        setTypeArray(typedArray)
+    }
+
+    lateinit var layout: LinearLayout
+    lateinit var symbol: ImageView
+    lateinit var snsText: TextView
+
+    private fun initView() {
+        val layoutInflater = context.getSystemService(Service.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = layoutInflater.inflate(R.layout.custom_sns_button, this, false)
+        addView(view)
+
+        layout = findViewById(R.id.ll_layout)
+        symbol = findViewById(R.id.iv_symbol)
+        snsText = findViewById(R.id.tv_sns_text)
+    }
+
+    private fun setTypeArray(typedArray: TypedArray) {
+        val backgroundColor = typedArray.getColor(
+            R.styleable.SnsButton_background_color,0)
+
+        layout.setBackgroundColor(backgroundColor)
+
+        val symbolDrawable = typedArray.getResourceId(
+            R.styleable.SnsButton_symbol_drawable,
+            R.drawable.ic_kakao_logo
+        )
+
+        symbol.setImageResource(symbolDrawable)
+
+        val text = typedArray.getText(R.styleable.SnsButton_text)
+        snsText.text = text
+
+        typedArray.recycle()
+    }
+}
