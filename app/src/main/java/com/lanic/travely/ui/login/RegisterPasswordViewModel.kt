48072f591b77126acd0630349616fe6e367b9606
lanic <takedawon@naver.com>
2021-06-22 23:55:17 +0900

    회원가입 파이어스토어 연동

--- app/src/main/java/com/lanic/travely/ui/login/RegisterPasswordViewModel.kt
+++ app/src/main/java/com/lanic/travely/ui/login/RegisterPasswordViewModel.kt
@@ -3,6 +3,7 @@ package com.lanic.travely.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.lanic.travely.R
 import com.lanic.travely.base.BaseViewModel
 import com.lanic.travely.data.RegisterData
@@ -20,8 +21,12 @@ class RegisterPasswordViewModel : BaseViewModel() {
     private val _state = MutableLiveData<Result>()
     val state: LiveData<Result> = _state
 
-    val auth: FirebaseAuth by lazy {
+    private val auth: FirebaseAuth by lazy {
         FirebaseAuth.getInstance()
+    }
+
+    private val db: FirebaseFirestore by lazy {
+        FirebaseFirestore.getInstance()
     }
 
     lateinit var registerData: RegisterData
@@ -42,9 +47,16 @@ class RegisterPasswordViewModel : BaseViewModel() {
         auth.createUserWithEmailAndPassword(registerData.email, registerData.password)
             .addOnSuccessListener {
                 _state.value = Result.Success
+                saveUserData(registerData)
             }
             .addOnFailureListener {
                 _state.value = Result.Failed
             }
+    }
+
+    private fun saveUserData(userData: RegisterData) {
+        db.collection("user")
+            .document(auth.uid!!)
+            .set(mapOf("nickname" to userData.nickname, "birth" to userData.birth))
     }
 }
